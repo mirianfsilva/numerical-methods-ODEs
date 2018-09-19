175536f4dede4da8376de8da1f6f2e50704d71e3
Mírian <mirianfrsilva@gmail.com>
2018-09-19 00:29:43 -0300

    Update ForwardEuler.py

--- ForwardEuler.py
+++ ForwardEuler.py
@@ -2,21 +2,17 @@
 import math, sys 
 import numpy as np
 
-def ForwardEuler(f, t0, y0, T, n, dt):
-    """Solve y'=f(t,y), y(0)=y0, with n steps until t=T."""
-    #div by zero
-    if dt == -1:
-        dt = (T-t0)/float(n)
-    else:
-        n = int(math.ceil((T-t0)/dt))
+def ForwardEuler(f, y0, T, n):
+    """Solve y’= f(y,t), y(0)=y0, with n steps until t=T."""
     t = np.zeros(n+1)
-    y = np.zeros(n+1)  # y[k] is the solution at time t[k]
+    y = np.zeros(n+1) # y[k] is the solution at time t[k]
     y[0] = y0
-    t[0] = t0
+    t[0] = 0
+    dt = T/float(n)
     for k in range(n):
         t[k+1] = t[k] + dt
-        y[k+1] = round(y[k] + dt*f(t[k], y[k]), 8)
-    return t, y
+        y[k+1] = y[k] + dt*f(y[k], t[k])
+    return y, t
 
 # Problem: y'=y
 def f(t, y): 
